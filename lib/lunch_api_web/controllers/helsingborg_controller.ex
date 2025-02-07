95ba6f2c29cef86fe8e31945d5ec43deb4d0fa49
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2025-02-07 22:48:18 +0100

    Respond to slack requests with plain text

--- lib/lunch_api_web/controllers/helsingborg_controller.ex
+++ lib/lunch_api_web/controllers/helsingborg_controller.ex
@@ -13,7 +13,11 @@ defmodule LunchApiWeb.HelsingborgController do
 
     menus = fetch_menus_concurrently(tasks)
 
-    json(conn, menus)
+    if slack_request?(conn) do
+      text(conn, format_for_slack(menus))
+    else
+      json(conn, menus)
+    end
   end
 
   def ramlosa(conn, _params) do
@@ -24,7 +28,11 @@ defmodule LunchApiWeb.HelsingborgController do
 
     menus = fetch_menus_concurrently(tasks)
 
-    json(conn, menus)
+    if slack_request?(conn) do
+      text(conn, format_for_slack(menus))
+    else
+      json(conn, menus)
+    end
   end
 
   def oceanhamnen(conn, _params) do
@@ -35,11 +43,35 @@ defmodule LunchApiWeb.HelsingborgController do
 
     menus = fetch_menus_concurrently(tasks)
 
-    json(conn, menus)
+    if slack_request?(conn) do
+      text(conn, format_for_slack(menus))
+    else
+      json(conn, menus)
+    end
   end
 
   defp fetch_menus_concurrently(tasks) do
     Enum.map(tasks, &Task.await/1)
     |> Enum.concat
+  end
+
+  defp slack_request?(conn) do
+    get_req_header(conn, "x-slack-signature") != [] and
+      get_req_header(conn, "x-slack-request-timestamp") != []
+  end
+
+  defp format_for_slack(menus) do
+    menus
+    |> Enum.map(fn menu ->
+      dishes_text = menu.dishes
+      |> Enum.map(fn dish ->
+        price_text = if dish.price != "", do: " (#{dish.price} kr)", else: ""
+        "- #{dish.dish}#{price_text}"
+      end)
+      |> Enum.join("\n")
+
+      "*#{menu.name}:*\n#{dishes_text}"
+    end)
+    |> Enum.join("\n\n")
   end
 end
