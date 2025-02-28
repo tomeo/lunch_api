a23b1be63bef453e2f1604b52571d29bd5063370
Tommy Ivarsson <tommy.ivarsson@gmail.com>
2025-03-01 00:11:44 +0100

    fix: Add cache to trelleborg and karlskrona

--- lib/lunch_api_web/controllers/karlskrona_controller.ex
+++ lib/lunch_api_web/controllers/karlskrona_controller.ex
@@ -1,14 +1,17 @@
 alias LunchApiWeb.Scrapers.Aggregators.MatOchMat
+alias LunchApi.MenuService
 
 defmodule LunchApiWeb.KarlskronaController do
   use LunchApiWeb, :controller
 
   def index(conn, _params) do
+    cache_key = "karlskrona_#{Date.utc_today()}"
+
     tasks = [
       Task.async(fn -> MatOchMat.city("karlskrona") end),
     ]
 
-    menus = LunchApi.MenuFetcher.fetch_menus_concurrently(tasks)
+    menus = MenuService.get_menus(cache_key, tasks)
 
     json(conn, menus)
   end
--- lib/lunch_api_web/controllers/trelleborg_controller.ex
+++ lib/lunch_api_web/controllers/trelleborg_controller.ex
@@ -1,14 +1,17 @@
 alias LunchApiWeb.Scrapers.Aggregators.MatOchMat
+alias LunchApi.MenuService
 
 defmodule LunchApiWeb.TrelleborgController do
   use LunchApiWeb, :controller
 
   def index(conn, _params) do
+    cache_key = "trelleborg_#{Date.utc_today()}"
+
     tasks = [
       Task.async(fn -> MatOchMat.city("trelleborg") end),
     ]
 
-    menus = LunchApi.MenuFetcher.fetch_menus_concurrently(tasks)
+    menus = MenuService.get_menus(cache_key, tasks)
 
     json(conn, menus)
   end
